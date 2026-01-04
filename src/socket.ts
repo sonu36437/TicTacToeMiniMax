068bdb3ac5ab469beaad197537406b6a0f20b868
sonu kumar <sonukumar@MacBookAir.lan>
2026-01-04 14:28:14 +0530

    minor fixes

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 const local ="http://localhost:3000"
-const online ="https://tictactoebackend-1.onrender.com/"
+const online ="https://tictactoebackend-1.onrender.com"
 export const socket= io(online,{
-    autoConnect:false
+    transports: ["websocket"],
+  upgrade: false,
+  autoConnect: false,
 })
