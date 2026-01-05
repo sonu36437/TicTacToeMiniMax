0709298b476f095553340edfd6e84a386bc19cbb
sonu kumar <sonukumar@MacBookAir.lan>
2026-01-05 09:23:05 +0530

    added railwayhosting url

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 const local ="http://localhost:3000"
 const online ="https://tictactoebackend-1.onrender.com"
-export const socket= io(online,{
+const railwayUrl="https://tictactoebackend-production-7055.up.railway.app/"
+export const socket= io(railwayUrl,{
     transports: ["websocket"],
   upgrade: false,
   autoConnect: false,
