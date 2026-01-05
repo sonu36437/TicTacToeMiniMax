039bae0459422f6254e5c9fa0c8f78a738491e06
sonu kumar <sonukumar@sonus-MacBook-Air.local>
2026-01-05 11:38:05 +0530

    changed server url

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 const local ="http://localhost:3000"
 const online ="https://tictactoebackend-1.onrender.com"
 const railwayUrl="https://tictactoebackend-production-7055.up.railway.app/"
-export const socket= io(railwayUrl,{
+export const socket= io(online,{
     transports: ["websocket"],
   upgrade: false,
   autoConnect: false,
